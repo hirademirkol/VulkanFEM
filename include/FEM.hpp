4b2a0cc1189e5797fe4189038f38e3c5947b9bef
Hira Demirkol <hirayigitdemirkol@gmail.com>
2023-07-21 00:39:21 +0200

    WIP: Integrated Eigen

--- include/FEM.hpp
+++ include/FEM.hpp
@@ -12,6 +12,11 @@
 #include <cs.h>
 #endif
 
+#define EIGEN
+#ifdef EIGEN
+#include <Eigen/Sparse>
+#endif
+
 #define MAX_ITER 20
 #define index(i,j,n) (i)*(n)+(j)
 #define get_symmetric(A,i,j) (i) <= (j) ? A[(i)][(j)] : A[(j)][(i)]
@@ -82,8 +87,11 @@ inline bool stationary(Vec3i element)
 }
 
 template<typename scalar>
-#ifdef CSPARSE
-cs* assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, scalar elementStiffness[24][24])
+// #ifdef CSPARSE
+// cs* assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, scalar elementStiffness[24][24])
+// #else
+#ifdef EIGEN
+Eigen::SparseMatrix<scalar> assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, scalar elementStiffness[24][24])
 #else
 Mat<scalar> assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, scalar elementStiffness[24][24])
 #endif
@@ -140,10 +148,14 @@ Mat<scalar> assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, sca
 
 	int size = usedVertices.size() * 3;
 
-#ifdef CSPARSE
-	cs* systemMatrix = new cs();
-	systemMatrix->m = size;
-	systemMatrix->n = size;
+// #ifdef CSPARSE
+// 	cs* systemMatrix = new cs();
+// 	systemMatrix->m = size;
+// 	systemMatrix->n = size;
+#ifdef EIGEN
+	Eigen::SparseMatrix<scalar> systemMatrix(size, size);
+	std::vector<Eigen::Triplet<scalar>> triplets;
+	// systemMatrix.reserve(75*size);
 #else
 	Mat<scalar> systemMatrix(size);
 #endif
@@ -164,8 +176,11 @@ Mat<scalar> assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, sca
 				{
 					for(int c2 = 0; c2 < 3; c2++)
 					{
-#ifdef CSPARSE
-						cs_entry(systemMatrix, iGlobal, jGlobal, get_symmetric(elementStiffness,i*3 + c1,j*3 + c2));
+// #ifdef CSPARSE
+// 						cs_entry(systemMatrix, iGlobal, jGlobal, get_symmetric(elementStiffness,i*3 + c1,j*3 + c2));
+#ifdef EIGEN
+						// systemMatrix.coeffRef(iGlobal*3 + c1, jGlobal*3 + c2) += get_symmetric(elementStiffness,i*3 + c1,j*3 + c2);
+						triplets.push_back(Eigen::Triplet<scalar>(iGlobal*3 + c1, jGlobal*3 + c2, get_symmetric(elementStiffness,i*3 + c1,j*3 + c2)));
 #else
 						systemMatrix[index(iGlobal*3 + c1, jGlobal*3 + c2, size)] += get_symmetric(elementStiffness,i*3 + c1,j*3 + c2);
 #endif
@@ -175,9 +190,13 @@ Mat<scalar> assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, sca
 		}
 	}
 
-#ifdef CSPARSE
-	cs_dupl(systemMatrix);
-	std::cout << "System Matrix : Size:" << size << "x" << size << ", Non-Zero:" << systemMatrix->nz << ", " << ((float)systemMatrix->nz/size) << " full per row" << std::endl;
+// #ifdef CSPARSE
+// 	cs_dupl(systemMatrix);
+// 	std::cout << "System Matrix : Size:" << size << "x" << size << ", Non-Zero:" << systemMatrix->nz << ", " << ((float)systemMatrix->nz/size) << " full per row" << std::endl;
+#ifdef EIGEN
+	systemMatrix.setFromTriplets(triplets.begin(), triplets.end());
+	// systemMatrix.makeCompressed();
+	std::cout << "System Matrix : Size:" << size << "x" << size << ", Non-Zero:" << systemMatrix.nonZeros() << ", " << ((float)systemMatrix.nonZeros()/size) << " full per row" << std::endl;
 #else
 	std::cout << "System Matrix : Size:" << size << "x" << size << ", Non-Zero:" << systemMatrix.data.size() << ", " << ((float)systemMatrix.data.size()/size) << " full per row" << std::endl;
 #endif
@@ -185,22 +204,33 @@ Mat<scalar> assembleSystemMatrix(int* voxelModel, Vec3i voxelGridDimensions, sca
 }
 
 template <typename scalar>
+#ifdef EIGEN
+void applyBoundaryConditions(Eigen::SparseMatrix<scalar>& systemMatrix, std::vector<scalar> f, const std::vector<uint64_t>& fixedNodes)
+#else
 void applyBoundaryConditions(Mat<scalar>& systemMatrix, std::vector<scalar> f, const std::vector<uint64_t>& fixedNodes)
+#endif
 {
-	
 	for(auto iter = fixedNodes.cend()-1; iter > fixedNodes.cbegin(); iter--)
 	{
 		// f.erase(*iter);
 
 		for(auto node : fixedNodes)
 		{
+			#ifdef EIGEN
+			// systemMatrix.coeffRef
+			#else
 			systemMatrix.data.erase(index(*iter, node, systemMatrix.rows));
+			#endif
 		}
 	}
+
+	#ifdef EIGEN
+	systemMatrix.makeCompressed();
+	#endif
 }
 
-template void applyBoundaryConditions<double>(Matd& systemMatrix, std::vector<double> f, const std::vector<uint64_t>& fixedNodes);
-template void applyBoundaryConditions<float>(Matf& systemMatrix, std::vector<float> f, const std::vector<uint64_t>& fixedNodes);
+// template void applyBoundaryConditions<double>(Matd& systemMatrix, std::vector<double> f, const std::vector<uint64_t>& fixedNodes);
+// template void applyBoundaryConditions<float>(Matf& systemMatrix, std::vector<float> f, const std::vector<uint64_t>& fixedNodes);
 
 template <typename scalar>
 std::vector<scalar> multiply(const Mat<scalar>& A, const std::vector<scalar>& b)
@@ -303,21 +333,146 @@ scalar two_norm(const std::vector<scalar>& b)
 	return sqrt(norm);
 }
 
-template <typename scalar>
-void smoothWithJacobi(const Mat<scalar>& A, const std::vector<scalar>& r, std::vector<scalar>& x)
-{
-	for(int i = 0; i < x.size(); i++)
+#ifdef EIGEN
+#undef index
+namespace Eigen {
+
+	template <typename scalar>
+	class MultigridPreconditioner
 	{
-		auto val = A[index(i,i,A.rows)];
-		if(val != 0.0)
-			x[i] += (scalar)0.4 * r[i] / val;
-	}
+		typedef Matrix<scalar, Dynamic, 1> Vector;
+
+		public:
+		typedef typename Vector::StorageIndex StorageIndex;
+		
+		enum {
+		ColsAtCompileTime = Dynamic,
+		MaxColsAtCompileTime = Dynamic
+		};
+
+		MultigridPreconditioner() : m_isInitialized(false) {}
+
+		explicit MultigridPreconditioner(const SparseMatrix<scalar>& mat) : m_invdiag(mat.cols())
+		{
+			compute(mat);
+		}
+
+		static void SetMatrices(const SparseMatrix<scalar> mat[2],
+										 const SparseMatrix<scalar> restriction[],
+										 const SparseMatrix<scalar> interpolation[],
+										 int numLevels)
+		{
+			MultigridPreconditioner::s_matrices = mat;
+			s_restrictionMatrices = restriction;
+			s_interpolationMatrices = interpolation;
+			s_numLevels = numLevels;
+		}
+
+
+		EIGEN_CONSTEXPR Index rows() const EIGEN_NOEXCEPT { return m_invdiag.size(); }
+		EIGEN_CONSTEXPR Index cols() const EIGEN_NOEXCEPT { return m_invdiag.size(); }
+
+		MultigridPreconditioner& analyzePattern(const SparseMatrix<scalar>& )
+		{
+			return *this;
+		}
+
+		MultigridPreconditioner& factorize(const SparseMatrix<scalar>& mat)
+		{
+			m_invdiag.resize(mat.cols());
+
+			for(int j=0; j<mat.outerSize(); ++j)
+			{
+				typename SparseMatrix<scalar>::InnerIterator it(mat,j);
+				while(it && it.index()!=j) ++it;
+				if(it && it.index()==j && it.value()!=scalar(0))
+				m_invdiag(j) = scalar(1)/it.value();
+				else
+				m_invdiag(j) = scalar(1);
+			}
+
+
+
+			m_isInitialized = true;
+			return *this;
+		}
+
+		MultigridPreconditioner& compute(const SparseMatrix<scalar>& mat)
+		{
+			return factorize(mat);
+		}
+
+		/** \internal */
+		void _solve_impl(const Vector& b, Vector& x) const
+		{
+			Vector r = b - s_matrices[0] * x;
+
+			r *= m_invdiag;
+
+			// for(int i = 0; i < m_numLevels)
+			// {
+			// 	r *= s_restrictionMatrices[i+1];
+			// }
+
+			// SimplicialLLT<SparseMatrix<scalar>> subSolver(m_matrix[2]);
+
+			// subSolver.solve(r);
+
+			// for(int i = 0; i < m_numLevels)
+			// {
+			// 	r *= s_interpolationMatrices[i+1];
+			// }
+
+			x += r;
+		}
+
+		template<typename Rhs> inline const Solve<MultigridPreconditioner, Rhs>
+		solve(const MatrixBase<Rhs>& b) const
+		{
+			eigen_assert(m_isInitialized && "MultigridPreconditioner is not initialized.");
+			eigen_assert(m_invdiag.size()==b.rows()
+						&& "MultigridPreconditioner::solve(): invalid number of rows of the right hand side matrix b");
+			return Solve<MultigridPreconditioner, Rhs>(*this, b.derived());
+		}
+
+		ComputationInfo info() { return Success; }
+
+		protected:
+		Vector m_invdiag;
+		bool m_isInitialized;
+
+		private:
+		static SparseMatrix<scalar>* s_matrices;
+		static SparseMatrix<scalar>* s_restrictionMatrices;
+		static SparseMatrix<scalar>* s_interpolationMatrices;
+		static int s_numLevels;
+	};
 }
 
-template void smoothWithJacobi<double>(const Mat<double>& A, const std::vector<double>& b, std::vector<double>& x);
-template void smoothWithJacobi<float>(const Mat<float>& A, const std::vector<float>& b, std::vector<float>& x);
+#define index(i,j,n) (i)*(n)+(j)
+#endif
 
 template <typename scalar>
+#ifdef EIGEN
+void solveWithCG(const Eigen::SparseMatrix<scalar>& A, const std::vector<scalar>& b, std::vector<scalar>& x, const std::vector<uint64_t> &fixedNodes)
+{
+	Eigen::Matrix<scalar, Eigen::Dynamic, 1> b_eig(b.size());
+	Eigen::Matrix<scalar, Eigen::Dynamic, 1> x_eig(x.size());
+
+	memcpy(b_eig.data(), b.data(), b.size()*sizeof(scalar)); 
+
+
+	// Eigen::MultigridPreconditioner<scalar>::SetMatrices();
+	Eigen::ConjugateGradient<Eigen::SparseMatrix<scalar>, 1, Eigen::MultigridPreconditioner<scalar>> solver(A);
+	
+	x_eig = solver.solve(b_eig);
+
+	std::cout << "#iterations:     " << solver.iterations() << std::endl;
+	std::cout << "estimated error: " << solver.error()      << std::endl;
+
+	std::memcpy(x.data(), x_eig.data(), x.size()*sizeof(scalar)); 
+}
+#else
 void solveWithCG(const Mat<scalar>& A, const std::vector<scalar>& b, std::vector<scalar>& x, const std::vector<uint64_t> &fixedNodes)
 {
 	uint64_t size = x.size();
@@ -362,6 +517,7 @@ void solveWithCG(const Mat<scalar>& A, const std::vector<scalar>& b, std::vector
 		std::cout << "iteration: " << i << ", " << "Norm: " << two_norm(r2) << std::endl;
 	}
 }
+#endif
 
-template void solveWithCG<double>(const Matd& A, const std::vector<double>& b, std::vector<double>& x, const std::vector<uint64_t> &fixedNodes);
-template void solveWithCG<float>(const Matf& A, const std::vector<float>& b, std::vector<float>& x, const std::vector<uint64_t> &fixedNodes);
+// template void solveWithCG<double>(const Matd& A, const std::vector<double>& b, std::vector<double>& x, const std::vector<uint64_t> &fixedNodes);
+// template void solveWithCG<float>(const Matf& A, const std::vector<float>& b, std::vector<float>& x, const std::vector<uint64_t> &fixedNodes);
