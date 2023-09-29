24435b76510aade1c0a9a5921b6c541056158af3
Hira Demirkol <hirayigitdemirkol@gmail.com>
2023-09-29 18:24:17 +0200

    First working kompute example

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <memory>
 #include <vector>
+#include <chrono>
 
-#define CPU
+// #define CPU
 
 #ifndef CPU
 #include <kompute/Kompute.hpp>
 
 // Compiled shaders
 #include "MatxVec.hpp"
+#include "FixedNodes.hpp"
+#include "VecDotVec.hpp"
 #include "GaussSeidel.hpp"
-#include "CG.hpp"
+#include "ConjugateGradient_1.hpp"
+#include "ConjugateGradient_2.hpp"
 
 typedef std::shared_ptr<kp::Tensor> Tensor;
 using TensorDataTypes = kp::Tensor::TensorDataTypes;
@@ -40,74 +44,10 @@ int main(int argc, char* argv[])
 	double Ke[24][24];
 	// ComputeKe(elementSize.x, elementSize.y, elementSize.z, 2e9, 0.394, Ke);
 	getKe(Ke);
-/*
-#pragma region // TODO: Check this and do the decoloring on the shader accordingly
-	// Preparation of grids
-	// int nActiveElements = elementGridSize.multiplyComponents();
-	// int nAcviteVertices = vertexGridSize.multiplyComponents();
-
-	// std::vector<int> elementGrid;
-	// std::vector<int> vertexGrid;
-	// std::vector<int8_t> vertexFixed; //boolean
-	// std::vector<int> vertexPosition;
-	// std::vector<float> vertexEnergy;
-	// std::vector<float> vertexMass;
-	// std::vector<float> vertexF;
-	// std::vector<float> vertexU;
-
-	// Prepare elementGrid and vertexGrid
-	Vec3i elementGridSize, vertexGridSize;
-
-	std::vector<int> coloredVertices;
-
-	int vert = 0;
-	Vec3i vi;
-	for (int color = 0; color < 8; color++)
-	{
-		coloredVertices.push_back(vert);
-		Vec3i vi0 = Vectorize(color, 2);
-		Vec3i vj0, vj1;
-
-		for (vi.z = vi0.z; vi.z < vertexGridSize.z; vi.z += 2)
-		{
-			vj0.z = std::max(-1, -vi.z);
-			vj1.z = std::min(1, elementGridSize.z - vi.z);
-
-			for (vi.y = vi0.y; vi.y < vertexGridSize.y; vi.y += 2)
-			{
-				vj0.y = std::max(-1, -vi.y);
-				vj1.y = std::min(1, elementGridSize.y - vi.y);
-
-				for (vi.x = vi0.x; vi.x < vertexGridSize.x; vi.x += 2)
-				{
-					vj0.x = std::max(-1, -vi.x);
-					vj1.x = std::min(1, elementGridSize.x - vi.x);
-					bool isVertexRequired = false;
-					Vec3i vj;
-					FOR3 (vj, vj0, vj1)
-					{
-						if (elementGrid[Linearize(vi + vj, elementGridSize)] != -1)
-						{
-							isVertexRequired = true;
-						}
-					}
-					if (isVertexRequired)
-					{
-						vertexGrid[Linearize(vi, vertexGridSize)] = vert;
-						vertexPosition[vert] = PackPosition(vi);
-						vert++;
-					}
-				}
-			}
-		}
-	}
-#pragma endregion
-*/
 
 	std::set<uint64_t> fixedNodes;
 	std::map<uint64_t, Vec3d> loadedNodes;
 	getBoundaryConditions(modelName, fixedNodes, loadedNodes);
-
 
 	auto systemMatrix = assembleSystemMatrix<double>(voxelModel, voxelModelSize, Ke, fixedNodes);
 
@@ -123,157 +63,170 @@ int main(int argc, char* argv[])
 	solveWithCG<double>(systemMatrix, f, u);
 #else
 
-	// float dt = 0.05f;
-	// int oneOverDt = (int)1/dt;
-
-	std::vector<double> ap;
-	ap.resize(numDoF, 0.0);
-
 	std::vector<double> r(f);
 	std::vector<double> p(f);
+	std::vector<double> Atp;
+	Atp.resize(numDoF, 0.0);
 
-	// Setting Kompute  
-
-	// Data for Matrix-free algorithm
-	/*
-	Tensor elementGridTensor = mgr.tensor((void*)elementGrid.data(), (uint64_t)nElements, sizeof(int), eInt, eStorage);
-	Tensor vertexGridTensor = mgr.tensor((void*)vertexGrid.data(), (uint64_t)nVertices, sizeof(int), eInt, eStorage);
-	Tensor vertexFixedTensor = mgr.tensor((void*)vertexFixed.data(), (uint64_t)nVertices, sizeof(bool), eBool, eStorage);
-	Tensor positionTensor = mgr.tensor((void*)vertexPosition.data(), (uint64_t)nVertices, sizeof(int), eInt, eStorage);
-	Tensor energyTensor = mgr.tensor((void*)vertexEnergy.data(), (uint64_t)nVertices, sizeof(float), eFloat, eStorage);
-	Tensor massTensor = mgr.tensor((void*)vertexMass.data(), (uint64_t)nVertices, sizeof(float), eFloat, eStorage);
-	Tensor KeTensor = mgr.tensor((void*)Ke, (uint64_t)(24*24), sizeof(double), eDouble, eStorage);
-	Tensor fTensor = mgr.tensor((void*)vertexF.data(), (uint64_t)nVertices, sizeof(float), eFloat, eDevice);
-	Tensor uTensor = mgr.tensor((void*)vertexU.data(), (uint64_t)nVertices, sizeof(float), eFloat, eDevice);
-
-	std::vector<int> constants{elementGridSize.x, 
-							elementGridSize.y,
-							elementGridSize.z,
-							vertexGridSize.x,
-							vertexGridSize.y, 
-							vertexGridSize.z,
-							nVertices,
-							oneOverDt};
-
-	
-	const std::vector<std::shared_ptr<kp::Tensor>> params = {elementGridTensor,
-															 vertexGridTensor,
-															 vertexFixedTensor,
-															 positionTensor,
-															 energyTensor,
-															 massTensor,
-															 KeTensor,
-															 fTensor,
-															 uTensor};
-
-	const std::vector<uint64_t> gaussSeidelShader = std::vector<uint64_t>(
-		shaders::GAUSSSEIDEL_COMP_SPV.begin(),	shaders::GAUSSSEIDEL_COMP_SPV.end());
-
-
-	std::vector<int> pushConstants{0};
-
-	int blockSize = 128;
-	// int numGroups
-	const kp::Workgroup workgroup({128, 128, 1});
-
-	std::shared_ptr<kp::Algorithm> algoGaussSeidel = mgr.algorithm<int, int>(params, gaussSeidelShader, workgroup, constants, pushConstants);
-
-	std::shared_ptr<kp::Sequence> seq = mgr.sequence();
-	
-	seq->record<kp::OpTensorSyncDevice>({uTensor, fTensor});
-	for(int i = 0; i < 8; i++)
-	{
-		kp::Constants color{(float)i};
-		seq->record<kp::OpAlgoDispatch>(algoGaussSeidel, color);
-		seq->record<kp::OpTensorSyncLocal>({uTensor, fTensor});
-	}
-
-	for(int i = 0; i < 20; i++)
-		seq->eval();
-	*/
-
+	// Setting Kompute
 	kp::Manager mgr(0, {}, { "VK_EXT_shader_atomic_float" });
 
-	// Data for Matrix based algorithm
-	auto indices = systemMatrix.indices();
-	auto values = systemMatrix.values();
-	uint64_t numIndices = indices.size();
+	// double *norm1, *norm2, *dotP, *zero;
 
-	double *d1, *n1, *n2;
-
-	d1 = new double(0.0);
-	n1 = new double(0.0);
-	n2 = new double(0.0);
+	double *norm1 = new double(0.0);
+	double *norm2 = new double(0.0);
+	double *dotP = new double(0.0);
+	double *zero = new double(0.0);
 
 	for(int i = 0; i < numDoF; i++)
-		*n2 += r[i] * r[i];
+		*norm2 += r[i] * r[i];
 
+	Eigen::Array<int, 8, Eigen::Dynamic> elementToNodeArray = systemMatrix.elementToNode.transpose();
 
-	Tensor systemMatrixIndices = mgr.tensor((void*)indices.data(), numIndices, sizeof(uint64_t), TensorDataTypes::eUnsignedInt, TensorTypes::eStorage);
-	Tensor systemMatrixValues = mgr.tensor((void*)values.data(), numIndices, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eStorage);
+	Tensor elementStiffnessTensor = mgr.tensor((void*)systemMatrix.elementStiffnessMat.data(), 24*24, sizeof(double), TensorDataTypes::eDouble);
+	Tensor elementToGlobalTensor = mgr.tensor((void*)elementToNodeArray.data(), systemMatrix.elementToNode.rows()*8, sizeof(int), TensorDataTypes::eInt);
+	Tensor fixedNodesTensor = mgr.tensor((void*)systemMatrix.fixedNodes.data(), systemMatrix.fixedNodes.rows(), sizeof(int), TensorDataTypes::eInt);
 	Tensor pTensor = mgr.tensor((void*)p.data(), (uint64_t)numDoF, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
-	Tensor apTensor = mgr.tensor((void*)ap.data(), (uint64_t)numDoF, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
+	Tensor AtpTensor = mgr.tensor((void*)Atp.data(), (uint64_t)numDoF, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
 	Tensor rTensor = mgr.tensor((void*)r.data(), (uint64_t)numDoF, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
-	Tensor fTensor = mgr.tensor((void*)f.data(), (uint64_t)numDoF, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
 	Tensor uTensor = mgr.tensor((void*)u.data(), (uint64_t)numDoF, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
-	Tensor d1Tensor = mgr.tensor((void*)d1, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
-	Tensor n1Tensor = mgr.tensor((void*)n1, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
-	Tensor n2Tensor = mgr.tensor((void*)n2, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
-	
+	Tensor norm1Tensor = mgr.tensor((void*)norm1, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
+	Tensor norm2Tensor = mgr.tensor((void*)norm2, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
+	Tensor dotPTensor = mgr.tensor((void*)dotP, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
+	Tensor zeroTensor = mgr.tensor((void*)zero, 1, sizeof(double), TensorDataTypes::eDouble, TensorTypes::eDevice);
 
-	const std::vector<Tensor> paramsMatxVec = {systemMatrixIndices,
-											   systemMatrixValues,
+
+	const std::vector<Tensor> paramsMatxVec = {elementStiffnessTensor,
+											   elementToGlobalTensor,
 											   pTensor,
-											   apTensor};
+											   AtpTensor};
 
-	const std::vector<Tensor> paramsCG = {pTensor,
-										  apTensor,
+	const std::vector<Tensor> paramsFixedNodes = {AtpTensor,
+											   fixedNodesTensor};
+
+	const std::vector<Tensor> paramsVecDotVec = {pTensor,
+										 		 AtpTensor,
+												 dotPTensor};
+
+	const std::vector<Tensor> paramsVecNorm = {rTensor,
+										 		 rTensor,
+												 norm2Tensor};
+
+	const std::vector<Tensor> paramsCG_1 = {pTensor,
+										  AtpTensor,
 										  rTensor,
-										  fTensor,
 										  uTensor,
-										  d1Tensor,
-										  n1Tensor,
-										  n2Tensor};
+										  norm2Tensor,
+										  dotPTensor};
 
-	const kp::Workgroup perNonZeroWorkgroup({numIndices, 1, 1});
-	const kp::Workgroup perDoFWorkgroup({numDoF, 1, 1});
+	const std::vector<Tensor> paramsCG_2 = {pTensor,
+										  rTensor,
+										  norm1Tensor,
+										  norm2Tensor};
+
+
+	// const kp::Workgroup perElementDoFWorkgroup({(uint32_t)systemMatrix.elementToNode.rows(), 24, 24});
+	const kp::Workgroup perElementDoFWorkgroup({(uint32_t)systemMatrix.elementToNode.rows(), 8, 8});
+	const kp::Workgroup perFixedNodeWorkgroup({(uint32_t)systemMatrix.fixedNodes.rows(), 1, 1});
+	const kp::Workgroup perDoFWorkgroup({(uint32_t)numDoF, 1, 1});
 
 	const std::vector<uint32_t> MatxVecShader = std::vector<uint32_t>(
 		shaders::MATXVEC_COMP_SPV.begin(),	shaders::MATXVEC_COMP_SPV.end());
 
-	const std::vector<uint32_t> CGShader = std::vector<uint32_t>(
-		shaders::CONJUGATEGRADIENT_COMP_SPV.begin(),	shaders::CONJUGATEGRADIENT_COMP_SPV.end());
+	const std::vector<uint32_t> FixedNodesShader = std::vector<uint32_t>(
+		shaders::FIXEDNODES_COMP_SPV.begin(),	shaders::FIXEDNODES_COMP_SPV.end());
 
-	std::shared_ptr<kp::Algorithm> algoMatxVec = mgr.algorithm<uint64_t, uint64_t>(paramsMatxVec, MatxVecShader, {}, {numDoF, numIndices}, {});
-	std::shared_ptr<kp::Algorithm> algoCG = mgr.algorithm(paramsCG, CGShader, perDoFWorkgroup);
+	const std::vector<uint32_t> VecDotVecShader = std::vector<uint32_t>(
+		shaders::VECDOTVEC_COMP_SPV.begin(),	shaders::VECDOTVEC_COMP_SPV.end());
 
-	std::shared_ptr<kp::Sequence> seq = mgr.sequence();
+	const std::vector<uint32_t> CGShader_1 = std::vector<uint32_t>(
+		shaders::CONJUGATEGRADIENT_1_COMP_SPV.begin(),	shaders::CONJUGATEGRADIENT_1_COMP_SPV.end());
 
-	seq->record<kp::OpTensorSyncDevice>({uTensor, fTensor, pTensor, rTensor, n2Tensor});
-	seq->record<kp::OpAlgoDispatch>(algoMatxVec);
-	seq->record<kp::OpTensorSyncLocal>({pTensor, apTensor});
-	// seq->record<kp::OpAlgoDispatch>(algoCG);
-	// seq->record<kp::OpTensorSyncLocal>({uTensor, fTensor, pTensor, rTensor, n2Tensor, apTensor});
+	const std::vector<uint32_t> CGShader_2 = std::vector<uint32_t>(
+		shaders::CONJUGATEGRADIENT_2_COMP_SPV.begin(),	shaders::CONJUGATEGRADIENT_2_COMP_SPV.end());
 
-	// for(int i = 0; i < MAX_ITER; i++)//while(n2 > tolerance)
-	// {
-		seq->eval();
-	// 	KP_LOG_INFO("Iteration: {}, Norm: {}", i, *n2);
-	// }
+	std::shared_ptr<kp::Algorithm> algoMatxVec = mgr.algorithm(paramsMatxVec, MatxVecShader, perElementDoFWorkgroup);
+	std::shared_ptr<kp::Algorithm> algoFixedNodes = mgr.algorithm(paramsFixedNodes, FixedNodesShader, perFixedNodeWorkgroup);
+	std::shared_ptr<kp::Algorithm> algoVecDotVec = mgr.algorithm(paramsVecDotVec, VecDotVecShader, perDoFWorkgroup);
+	std::shared_ptr<kp::Algorithm> algoVecNorm = mgr.algorithm(paramsVecNorm, VecDotVecShader, perDoFWorkgroup);
+	std::shared_ptr<kp::Algorithm> algoCG_1 = mgr.algorithm(paramsCG_1, CGShader_1, perDoFWorkgroup);
+	std::shared_ptr<kp::Algorithm> algoCG_2 = mgr.algorithm(paramsCG_2, CGShader_2, perDoFWorkgroup);
 
-	ap.assign(apTensor->data<double>(), apTensor->data<double>() + numDoF);
-	// saveMatrix(systemMatrix.data, numDoF, "A");
-	// saveMatrix(std::vector<std::vector<double>>({p}), "p");
-	saveMatrix(std::vector<std::vector<double>>({ap}), "ap");
+	mgr.sequence()->eval<kp::OpTensorSyncDevice>(paramsMatxVec);
+	mgr.sequence()->eval<kp::OpTensorSyncDevice>(paramsCG_1);
+	mgr.sequence()->eval<kp::OpTensorSyncDevice>(paramsCG_2);
 
-	delete d1, n1, n2;
+	std::shared_ptr<kp::Sequence> seq1 =
+		mgr.sequence()->record<kp::OpTensorSyncDevice>({AtpTensor, dotPTensor})
+					  ->record<kp::OpAlgoDispatch>(algoMatxVec)
+					  ->record<kp::OpAlgoDispatch>(algoFixedNodes)
+					  ->record<kp::OpAlgoDispatch>(algoVecDotVec)
+					  ->record<kp::OpAlgoDispatch>(algoCG_1)
+				  	  ->record<kp::OpTensorSyncLocal>({AtpTensor, dotPTensor, elementToGlobalTensor});
+
+	std::shared_ptr<kp::Sequence> seq2 =
+		mgr.sequence()->record<kp::OpTensorSyncDevice>({norm1Tensor, norm2Tensor})
+					  ->record<kp::OpAlgoDispatch>(algoVecNorm)
+				  	  ->record<kp::OpTensorSyncLocal>({norm2Tensor});
+
+	std::shared_ptr<kp::Sequence> seq3 =
+		mgr.sequence()->record<kp::OpAlgoDispatch>(algoCG_2)
+					  ->record<kp::OpTensorSyncLocal>({pTensor});
+
+
+#ifdef MAX_ITER
+	int maxIterations = MAX_ITER;
+#else
+	int maxIterations = 4000;
+#endif
+
+#ifdef TOLERANCE
+	double tolerance = TOLERANCE;
+#else
+	double tolerance = 1e-6;
+#endif
+
+	double threshold = tolerance * tolerance * *norm2;
+
+	std::cout << "Starting the GPU Solver" << std::endl;
+
+	int iter;
+	auto start = std::chrono::system_clock::now();
+	for(iter = 0; iter < maxIterations; iter++)
+	{
+		memcpy(AtpTensor->data<double>(), Atp.data(), Atp.size()*sizeof(double));
+		*dotPTensor->data<double>() = 0.0;
+		seq1->eval();
+
+		*norm1Tensor->data<double>() = *norm2Tensor->data<double>();
+		*norm2Tensor->data<double>() = 0.0;
+
+		seq2->eval();
+		// std::cout << "Iteration: "<< i <<", Norm: " << (*norm2Tensor->data<double>()) << std::endl;
+		if(*norm2Tensor->data<double>() < threshold)
+		{
+			break;
+		}
+
+		seq3->eval();
+	}
+	auto end = std::chrono::system_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+	
+	std::cout << "Solving took:    " << duration.count() << " s" 					<< std::endl;
+	std::cout << "#iterations:     " << iter	    								<< std::endl;
+	std::cout << "Estimated error: " << sqrt(*norm2Tensor->data<double>() / *norm2)	<< std::endl;
+
+	mgr.sequence()->eval<kp::OpTensorSyncLocal>({uTensor});
+	u = uTensor->vector<double>();
+	delete norm2, dotP;
+
 #endif
 
 	delete voxelModel;
 
 	std::cout << "Saving the solution" << std::endl;
 	saveVector(u, modelName);
-	// printModel(system.data(), vertexGridSize.multiplyComponents()*vertexGridSize.multiplyComponents());
 
 	return EXIT_SUCCESS;
 }
